eae44fd1045df40a3b108bba1f6e33c2aa0a3db3
justin.jarrard75@gmail.com <justin.jarrard75@gmail.com>
2022-01-18 23:29:57 -0500

    Added death logic to monsters as well as a poof particle animation when monsters are destroyed. Finally added a basic level controller and set up a Level1 and Level2 to test functionality

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private GameObject _cloudParticlePrefab;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -12,6 +13,7 @@ public class Enemy : MonoBehaviour
 
         if (bird != null)
         {
+            Instantiate(_cloudParticlePrefab,transform.position, Quaternion.identity);
             Destroy(gameObject);
             return;
         }
@@ -24,6 +26,8 @@ public class Enemy : MonoBehaviour
 
         if (collision.contacts[0].normal.y < -0.5)
         {
+            
+            Instantiate(_cloudParticlePrefab,transform.position, Quaternion.identity);
             Destroy(gameObject);
             return;
         }
